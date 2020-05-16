4d96a5b8e3794c5d69499ecda69f2afd7c4c854d
vasiliy1999 <vkolyasev1999@mail.ru>
2020-05-16 16:03:41 +0300

    SQL

---
+++ train6_images.sql
@@ -0,0 +1,12 @@
+create table images
+(
+    id        int auto_increment
+        primary key,
+    name      varchar(100) not null,
+    extension varchar(30)  not null
+);
+
+INSERT INTO train6.images (id, name, extension) VALUES (27, '_bIniVXdXs', 'jpg');
+INSERT INTO train6.images (id, name, extension) VALUES (28, '_MyyorXEq', 'jpg');
+INSERT INTO train6.images (id, name, extension) VALUES (29, '_LvRPamQJXc', 'jpg');
+INSERT INTO train6.images (id, name, extension) VALUES (30, '_MeoCB_amg', 'jpg');
